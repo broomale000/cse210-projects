12b46fcb88d8490d4740cfa02443670adb460874
broomale000 <bro24013@byui.edu>
2025-01-15 23:04:07 -0700

    uptoPrep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program! ");
+        }
+        DisplayMessage();
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        string returnedName = PromptUserName();
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number? ");
+            string userNumber = Console.ReadLine();
+            int userProcessedNumber = int.Parse(userNumber);
+            int squaredNum = userProcessedNumber * userProcessedNumber;
+            return squaredNum;
+        }
+        int returnedSquare = PromptUserNumber();
+
+        Console.WriteLine("Input a number to square: ");
+        string userSquareRawInput = Console.ReadLine();
+        int userSquareProcessedInput = int.Parse(userSquareRawInput);
+
+        static int SquareNumber(int number)
+        {
+            int product = number * number;
+            return product;
+        }
+
+
+
+        static void DisplayResult(string returnedName, int returnedSquare)
+        {
+            Console.WriteLine($"This is displaying your name: {returnedName}");
+            Console.WriteLine($"This is displaying your favorite number squared: {returnedSquare}");
+        }
+        DisplayResult(returnedName, returnedSquare);
+
     }
+    
 }
